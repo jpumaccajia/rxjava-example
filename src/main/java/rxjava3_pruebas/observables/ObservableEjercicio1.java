4c482532deea8cae6e41733cf1100c15f690faaf
Javier Serapio Pumaccajia Tasilla <jpumacca@emeal.nttdata.com>
2023-07-12 16:05:24 -0500

    Ejercicio1

--- src/main/java/rxjava3_pruebas/observables/ObservableEjercicio1.java
+++ src/main/java/rxjava3_pruebas/observables/ObservableEjercicio1.java
@@ -23,31 +23,13 @@ public class ObservableEjercicio1 {
         observableString = observableString.filter(x -> x.toUpperCase().startsWith("A"))
                 .map(y -> y.toUpperCase());
 
-//        2. Observador
-        Observer<String> observerString = new Observer<String>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                System.out.println("onSubscribe");
-            }
-
-            @Override
-            public void onNext(@NonNull String s) {
-                System.out.println("onNext: " + s);
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                System.out.println("onError");
-            }
-
-            @Override
-            public void onComplete() {
-                System.out.println("onComplete");
-            }
-        };
 
 //        3. Suscriptor
-        observableString.subscribe(observerString);
+        observableString.subscribe(
+                nombre -> System.out.println("onNext " + nombre),
+                error -> System.out.println("error: " + error.getMessage()),
+                () -> System.out.println("onComplete")
+        );
     }
 
 }
